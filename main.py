4b181c6a86837c013d1d218b75e37458fd2b36f4
Dirkanan <lohmatovurij0@gmail.com>
2024-06-18 19:57:30 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+# 1 задание
+a='Юля любит яблоки'
+print(len(a))
+# 2 задание
+first=12
+second=15
+summa=first+second
+diff=first-second
+print(summa)
+print(diff)
+# 3 задание
+first=12
+second=15
+third=140
+mean=(first+second+third)/3
+print(mean)
+# 4 задание
+first_string= "Понедельник"
+second_string= "Вторник"
+print(first_string+', ' + second_string)
+# 5 задание
+a= 12
+b= 4
+c=9
+f=(((a*b)+(a*c))^3)/2
+print(f)
