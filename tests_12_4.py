3446af21c1bbadfe3e684dc497916e96feea4c32
Dirkanan <lohmatovurij0@gmail.com>
2024-11-22 20:15:33 +0500

    Create tests_12_4.py

---
+++ tests_12_4.py
@@ -0,0 +1,31 @@
+import unittest
+import logging
+from runner import Runner
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename='runner_tests.log',
+    filemode='w',
+    encoding='utf-8',
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        try:
+            runner = Runner('Тестовый Бегун', -12)
+            runner.walk()
+            logging.info('"test_walk" выполнен успешно')
+        except ValueError as e:
+            logging.warning('Неверная скорость для Runner', exc_info=True)
+
+    def test_run(self):
+        try:
+            runner = Runner(True, 10)
+            runner.run()
+            logging.info('"test_run" выполнен успешно')
+        except TypeError as e:
+            logging.warning('Неверный тип данных для объекта Runner', exc_info=True)
+
+if __name__ == '__main__':
+    unittest.main()
