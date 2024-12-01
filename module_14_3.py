0da6bd3809dc23cca64e617e0f188b1f7fd7a903
Dirkanan <lohmatovurij0@gmail.com>
2024-12-01 22:19:48 +0500

    Update module_14_3.py

--- module_14_3.py
+++ module_14_3.py
@@ -54,6 +54,7 @@ async def get_buying_list(message):
 @dp.callback_query_handler(text='product_buying')
 async def send_confirm_message(call):
     await call.message.answer("Вы успешно приобрели продукт!")
+    await call.answer ()
 
 @dp.callback_query_handler(text='calories')
 async def set_age(call):
