6e82346c8df888907d915aa7e98c81cdf11a3c56
Dirkanan <lohmatovurij0@gmail.com>
2025-02-02 15:32:23 +0500

    Create main_1.py

---
+++ main_1.py
@@ -0,0 +1,19 @@
+# 1st program
+print(9**0.5*5)
+# 2nd program
+
+print((9.99 > 9.98) and (1000 != 1000.1))
+# 3rd program
+a = 2 * 2 + 2
+b = 2 * (2 + 2)
+c = a == b
+print(a)
+print(b)
+print(c)
+# 4th program
+string_value = '123.456'
+float_value = float(string_value)  
+shifted_value = float_value * 10   
+int_value = int(shifted_value)
+first_digit_after_decimal = int_value % 10  
+print(first_digit_after_decimal)
